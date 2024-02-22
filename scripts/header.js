38f349bbe05598007a6ecf83311c81dad6317fef
SimeJadrijev <sime.jadrijev@gmail.com>
2024-02-22 18:49:37 +0100

    Finished the header for mobile version

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,23 @@
 const menuButton = document.getElementById("hamburger-menu");
+const closeButton = document.getElementById("close-icon");
+const mobileNavigation = document.querySelector(".mobile-navigation")
 menuButton.addEventListener("click", () => {
-    console.log("yes")
+    menuButton.style.display = "none";
+    closeButton.style.display = "flex";
+    mobileNavigation.style.display = "flex";
+})
+
+closeButton.addEventListener("click", () => {
+    menuButton.style.display = "flex";
+    closeButton.style.display = "none";
+    mobileNavigation.style.display = "none";
+})
+
+const menuOptions = document.querySelectorAll(".mobile-navigation");
+menuOptions.forEach( (element) => {
+    element.addEventListener("click", () => {
+        menuButton.style.display = "flex";
+        closeButton.style.display = "none";
+        mobileNavigation.style.display = "none";
+    })
 })
